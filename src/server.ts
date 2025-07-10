3bf0c0a75322156572acab7623926ab757e075a5
Fabiano Pereira <fabianop971@gmail.com>
2025-07-10 18:37:04 -0300

    Feat: finish interface

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import { fastifyCors } from "@fastify/cors"
+import { fastifyMultipart } from "@fastify/multipart"
 import { fastify } from "fastify"
 import {
   serializerCompiler,
@@ -10,6 +11,7 @@ import { CreateQuestion } from "./routes/create-question.ts"
 import { CreateRoom } from "./routes/create-room.ts"
 import { GetRoomQuestions } from "./routes/get-room-questions.ts"
 import { GetRooms } from "./routes/get-rooms.ts"
+import { UploadAudio } from "./routes/upload-audio.ts"
 
 const server = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -20,10 +22,11 @@ server.setSerializerCompiler(serializerCompiler)
 server.setValidatorCompiler(validatorCompiler)
 
 server.get("/health", () => "ok")
-
+server.register(fastifyMultipart)
 server.register(GetRooms)
 server.register(CreateRoom)
 server.register(GetRoomQuestions)
 server.register(CreateQuestion)
+server.register(UploadAudio)
 
 server.listen({ port: env.PORT })
